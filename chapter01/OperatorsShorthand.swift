c1bfbc68c149b1d8d594b9159f9d90ae1fe4d15c
Waihon Yew <yewwaihon@gmail.com>
2020-12-06 12:29:36 +0800

    Create OperatorsShorthand.swift

---
+++ chapter01/OperatorsShorthand.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+var counter: Int = 8
+print(counter)
+
+// Shorthand for increment
+counter += 1
+print(counter)  // 9
+
+// Shorthand for decrement
+counter -= 1
+print(counter)  // 8
+
+// Shorthand for multiplication
+counter *= 3
+print(counter)  // 24
+
+// Shorthand for division
+counter /= 2
+print(counter)  // 12
