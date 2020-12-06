45d9263b241c6291d3ad4aa574b709222e05d668
Waihon Yew <yewwaihon@gmail.com>
2020-12-06 11:42:11 +0800

    Create naming-data.swift

---
+++ chapter01/naming-data.swift
@@ -0,0 +1,18 @@
+// Naming Data
+import Foundation
+
+// Constants
+let number: Int = 10
+let dPi: Double = 3.1415926535897932384626433
+let fPi: Float =  3.1415926535897932384626433
+
+print(number)   // 10
+print(dPi)      // 3.141592653589793
+print(fPi)      // 3.1415927
+
+// Variables
+var variableNumber: Int = 42
+variableNumber = 0
+variableNumber = 1_000_000
+
+print(variableNumber)   // 1000000
