8d6aae3a557364a6b287526a906cf6ae379d174d
Waihon Yew <yewwaihon@gmail.com>
2020-12-08 22:15:02 +0800

    Create Challenge07.swift

---
+++ Chapter01/Challenge07.swift
@@ -0,0 +1,10 @@
+// Challenge 7: Electrical resistance
+
+import Foundation
+
+let voltage: Double = 40.218
+let current: Double = 74.651
+let power: Double = voltage * current
+let resistance: Double = power / current.squareRoot()
+
+print(resistance)    // 347.4867783875007
