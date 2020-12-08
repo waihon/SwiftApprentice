cf3c4a67ad94b85a384ac62ab25ea12371258dd4
Waihon Yew <yewwaihon@gmail.com>
2020-12-08 22:08:34 +0800

    Create Challenge05.swift

---
+++ Chapter01/Challenge05.swift
@@ -0,0 +1,10 @@
+// Challenge 5: Average rating
+
+import Foundation
+
+let rating1: Double = 74_662.636218522
+let rating2: Double = 32_403.368304265
+let rating3: Double = 33_898.056892917
+let averageRating: Double = (rating1 + rating2 + rating3) / 3.0
+
+print(averageRating)
