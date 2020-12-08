c3d106dc9b687fdb80bbaf17de665c3355d288bf
Waihon Yew <yewwaihon@gmail.com>
2020-12-08 22:01:15 +0800

    Create Challenge03.swift

---
+++ Chapter01/Challenge03.swift
@@ -0,0 +1,18 @@
+// Challenge 3: Compute the answer
+
+import Foundation
+
+let x: Int = 46
+let y: Int = 10
+
+// 1
+let answer1: Int = (x * 100 + y)    // 4610
+print(answer1)
+
+// 2
+let answer2: Int = (x * 100) + (y * 100)    // 5600
+print(answer2)
+
+// 3
+let answer3: Int = (x * 100) + (y / 10)     // 4601
+print(answer3)
