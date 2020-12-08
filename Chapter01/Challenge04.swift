2e6182965f84d18d63e49b2ee19da693a92c3438
Waihon Yew <yewwaihon@gmail.com>
2020-12-08 22:03:13 +0800

    Create Challenge04.swift

---
+++ Chapter01/Challenge04.swift
@@ -0,0 +1,9 @@
+// Challenge 4: Add parentheses
+
+import Foundation
+
+let answer1: Int = 8 - 4 * 2 + 6 / 3 * 4
+print(answer1)  // 8
+
+let answer2: Int = 8 - (4 * 2) + ((6 / 3) * 4)
+print(answer2)  // 8
