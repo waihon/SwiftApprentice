3dcdf8f28c90ab5b63d0830774de0c63fbf01de7
Waihon Yew <yewwaihon@gmail.com>
2020-12-13 00:02:45 +0800

    Create TypeInference.swift

---
+++ Chapter02/TypeInference.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let inferredInt = 48
+let inferredDouble = 3.14159265359
+
+print(inferredInt)
+print(inferredDouble)
+
+let convertDouble = Double(3)
+let asDouble = 3 as Double
+
+print(convertDouble)
+print(asDouble)
