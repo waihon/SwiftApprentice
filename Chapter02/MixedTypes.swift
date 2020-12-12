48ed8af0ae88914a1c9a77f398ecf200c67b13c8
Waihon Yew <yewwaihon@gmail.com>
2020-12-12 19:39:36 +0800

    Create MixedTypes.swift

---
+++ Chapter02/MixedTypes.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let hourlyRate: Double = 39.75
+let hoursWorked: Int = 40
+let totalCost: Double = hourlyRate * Double(hoursWorked)
+
+print(hourlyRate)   // 39.75
+print(hoursWorked)  // 20
+print(totalCost)    // 1590.0
