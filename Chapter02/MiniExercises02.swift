c16106109064ed6200b727507eddf79e47723445
Waihon Yew <yewwaihon@gmail.com>
2020-12-13 19:41:16 +0800

    Create MiniExercises02.swift

---
+++ Chapter02/MiniExercises02.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let firstName = "Waihon"
+let lastName = "Yew"
+
+let fullName = firstName + " " + lastName
+let myDetails = "Hello, my name is \(fullName)."
+
+print(myDetails)    // Hello, my name is Waihon Yew.
