a3ee84508ec1986d045b119ab8eac2c97681156d
Waihon Yew <yewwaihon@gmail.com>
2020-12-13 19:24:06 +0800

    Create MultilineStrings.swift

---
+++ Chapter02/MultilineStrings.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let multilineString = """
+    You can have a string
+    that contains multiple lines
+    by doing this.
+    """
+print(multilineString)
+// You can have a string
+// that contains multiple lines
+// by doing this.
+
+let codeSnippet = """
+    func sayHello(name: String) {
+        print("Hello, \\(name)!")   
+    }
+    """
+print(codeSnippet)
+// func sayHello(name: String) {
+//    print("Hello, \(name)!")   
+// }
