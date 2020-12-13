41081f91d2cddb78bc1214017c595fb906672505
Waihon Yew <yewwaihon@gmail.com>
2020-12-13 18:47:20 +0800

    Create Interpolation.swift

---
+++ Chapter02/Interpolation.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let name = "Waihon"
+var message = "Hello! " + "My name is \(name)!"
+print(message)  // Hello! May name is Waihon!
+
+let oneThird = 1.0 / 3.0
+let oneThirdString = "One third is \(oneThird) as a decimal."
+print(oneThirdString)   // One third is 0.3333333333333333 as a decimal.
