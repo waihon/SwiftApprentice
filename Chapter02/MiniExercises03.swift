38d673722083e5025168d6b7ff6a54ddc465a8ff
Waihon Yew <yewwaihon@gmail.com>
2020-12-13 22:31:17 +0800

    Create MiniExercises03.swift

---
+++ Chapter02/MiniExercises03.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// Defining tuples
+var dateAverageTemperature = (month: 12, day: 13, year: 2020, averageTemperature: 29.6)
+print(dateAverageTemperature)   
+// (month: 12, day: 13, year: 2020, averageTemperature: 29.6)
+
+let (_, day, _, averageTemperature) = dateAverageTemperature
+print(day)                  // 13
+print(averageTemperature)   // 29.6
+
+dateAverageTemperature.averageTemperature = 30.4
+print(dateAverageTemperature)   
+// (month: 12, day: 13, year: 2020, averageTemperature: 29.6)
