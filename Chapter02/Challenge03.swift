0e3eae8580a5ba3455cf462cf61dc005d3ddfec3
Waihon Yew <yewwaihon@gmail.com>
2020-12-14 22:41:14 +0800

    Create Challenge03.swift

---
+++ Chapter02/Challenge03.swift
@@ -0,0 +1,10 @@
+// Challenge 3: Which are valid?
+
+import Foundation
+
+// Invalid - cannot convert value of type 'String' to specified type 'Character'
+//let character: Character = "Dog"
+
+let character: Character = "🐶"     // Valid
+let string: String = "Dog"          // Valid
+let emoji: String = "🐶"            // Valid
