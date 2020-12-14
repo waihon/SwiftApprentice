4ab3e0de75836ea6539270c1330564d29a6768d3
Waihon Yew <yewwaihon@gmail.com>
2020-12-14 22:52:44 +0800

    Create Challenge08.swift

---
+++ Chapter02/Challenge08.swift
@@ -0,0 +1,8 @@
+// Challenge 8: What is the value of summary?
+
+import Foundation
+
+let number = 10
+let multiplier = 5
+let summary = "\(number) multiplied by \(multiplier) equals \(number * multiplier)."
+// 10 multiplied by 5 equals 50.
